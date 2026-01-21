1158d88bf3af41aec3ccafef91d1eef6e0643363
Hembo Pagi <hembo.pagi@artun.ee>
2026-01-21 17:12:19 +0200

    v1.5.5 update

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,5 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_1gf2G8 from "../assets/firefox_seadistamine.png?astroContentImageFlag=&importer=src%2Fpages%2Fkirjutised%2Fv1_5_5.md";
+import __ASTRO_IMAGE_IMPORT_JCsy4 from "../../assets/firefox_seadistamine.png?astroContentImageFlag=&importer=src%2Fpages%2Fkirjutised%2Fv1_5_5.md";
+export default new Map([["../assets/firefox_seadistamine.png?astroContentImageFlag=&importer=src%2Fpages%2Fkirjutised%2Fv1_5_5.md", __ASTRO_IMAGE_IMPORT_1gf2G8], ["../../assets/firefox_seadistamine.png?astroContentImageFlag=&importer=src%2Fpages%2Fkirjutised%2Fv1_5_5.md", __ASTRO_IMAGE_IMPORT_JCsy4]]);
+		
