7f23c25b564ca878dad8d30913183c782c7f7135
Hembo Pagi <hembo.pagi@artun.ee>
2026-01-26 16:32:22 +0200

    printeri õiguste määramine

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,4 @@
 
 export default new Map([
-["src/pages/kirjutised/iiif-tugi.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fkirjutised%2Fiiif-tugi.mdx&astroContentModuleFlag=true")],
 ["src/pages/kirjutised/v1_5_5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fpages%2Fkirjutised%2Fv1_5_5.mdx&astroContentModuleFlag=true")]]);
 		
